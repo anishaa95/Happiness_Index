e930c0c238d8be4a427818d28cf5200b2811a86b
Joseph Geglia <jmgeglia@gmail.com>
2020-04-25 12:07:01 -0400

    adding updated map html

--- map_config.js
+++ map_config.js
@@ -1,6 +1,2 @@
 // API key
-<<<<<<< HEAD
 const API_KEY = "pk.eyJ1IjoiamdnbGlhIiwiYSI6ImNrOHJ4a3N5ZjBhcDIzZG16MDFpZHppb3MifQ.L7v6KnuawGagjM6HHbZXpQ";
-=======
-const API_KEY = "Put your API Key here";
->>>>>>> 557bdc834d02e12f21b6db13b89971160a17eaca

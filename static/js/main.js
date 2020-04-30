89324bdd04d734c86d76d9526c6c01592d368a51
Marciooliver <marcio.tov@gmail.com>
2020-04-30 00:08:14 -0400

    JS with Variables

--- static/js/main.js
+++ static/js/main.js
@@ -1,57 +1,73 @@
 var dataset = {};
 
-d3.json("/api/v1.0/happyness_index", function (data) {
-  console.log(data);
-  dataset = data;
-});
-
-d3.select(sarah).on("click", handleSubmit);
+let element = document.querySelector('button');
+element.onclick = handleSubmit;
+// d3.select(sarah).on("click", handleSubmit);
 
 // Code from Reed
 function handleSubmit(event) {
-	//add the rest of variables
-  const soc_gdp_percapita = document.getElementById("soc_gdp_percapita");
-  const soc_suicide_per_100K = document.getElementById("soc_suicide_per_100K");
-
-  //retrieve your slider variables and scale them
-  // 	25% by 3 sliders
-  var s_gdp = 25 - soc_gdp_percapita.value * 2.5;
-  var s_lit = 25 - (soc_literacy_ % pop.value) * 2.5;
-  var s_sd = 25 - soc_suicide_per_100K.value * 2.5;
-  var s_br = 25 - soc_birthrate_cbr.value * 2.5;
-  // 25% by 2 sliders
-  var f_ag = 25 - (eco_ % agriculture.value) * 2.5;
-  var f_ind = 25 - (eco_ % industry.value) * 2.5;
-  var f_ser = 25 - (eco_ % service.value) * 2.5;
-  var f_sel = 25 - (eco_ % self_employed.value) * 2.5;
-  // 25% by 3 sliders
-  var m_pop = 25 - (travel_popdensity_ / sqmile.value) * 2.5;
-  var m_cos = 25 - (travel_coastline_ % ratio.value) * 2.5;
-  var m_inc = 25 - (travel_incountry_ % growth.value) * 2.5;
-  var m_arr = 25 - travel_no_of_arrivals.value * 2.5;
-  //  25% by 2 sliders
-  var c_fem = 25 - (pop_ % female.value) * 2.5;
-  var c_avg = 25 - (pop_ % avghousehold_size.value) * 2.5;
-  var c_typ = 25 - (pop_ % householdtype_nuclear.value) * 2.5;
-  var c_noc = 25 - (pop_ % householdtype_nochildren.value) * 2.5;
-
-  // perform your magical calculation
-
-
-
-  bestCountriestoLiveIn = dataset.filter( c => c.happyscore >= ourscore );
-
-  let magical_calculation_results = {
-	bestCountriestoLiveIn: bestCountriestoLiveIn,
-	geoDataforMap: {}
-  };
-  
-  // once you're done ...
+	const soc_gdp_percapita = document.getElementById("soc_gdp_percapita");
+	const soc_literacy_pop = document.getElementById("soc_literacy_pop");
+	const soc_suicide_per_100K = document.getElementById("soc_suicide_per_100K");
+	const soc_birthrate_cbr = document.getElementById("soc_birthrate_cbr");
+	const eco_agriculture = document.getElementById("eco_agriculture");
+	const eco_industry = document.getElementById("eco_industry");
+	const eco_service = document.getElementById("eco_service");
+	const eco_self_employed = document.getElementById("eco_self_employed");
+	const travel_popdensity_sqmile = document.getElementById("travel_popdensity_sqmile");
+	const travel_coastline_ratio = document.getElementById("travel_coastline_ratio");
+	const travel_incountry_growth = document.getElementById("travel_incountry_growth");
+	const travel_no_of_arrivals = document.getElementById("travel_no_of_arrivals");
+	const pop_female = document.getElementById("pop_female");
+	const pop_avghousehold_size = document.getElementById("pop_avghousehold_size");
+	const pop_householdtype_nuclear = document.getElementById("pop_householdtype_nuclear");
+	const pop_householdtype_nochildren = document.getElementById("pop_householdtype_nochildren");
+	
+	//retrieve your slider variables and scale them
+	// 	25% by 3 sliders
+	var s_gdp = 25 - soc_gdp_percapita.value * 2.5;
+	var s_lit = 25 - soc_literacy_pop.value * 2.5;
+	var s_sd = 25 - soc_suicide_per_100K.value * 2.5;
+	var s_br = 25 - soc_birthrate_cbr.value * 2.5;
+	// 25% by 2 sliders
+	var f_ag = 25 - eco_agriculture.value * 2.5;
+	var f_ind = 25 - eco_industry.value * 2.5;
+	var f_ser = 25 - eco_service.value * 2.5;
+	var f_sel = 25 - eco_self_employed.value * 2.5;
+	// 25% by 3 sliders
+	var m_pop = 25 - travel_popdensity_sqmile.value * 2.5;
+	var m_cos = 25 - travel_coastline_ratio.value * 2.5;
+	var m_inc = 25 - travel_incountry_growth.value * 2.5;
+	var m_arr = 25 - travel_no_of_arrivals.value * 2.5;
+	//  25% by 2 sliders
+	var c_fem = 25 - pop_female.value * 2.5;
+	var c_avg = 25 - pop_avghousehold_size.value * 2.5;
+	var c_typ = 25 - pop_householdtype_nuclear.value * 2.5;
+	var c_noc = 25 - pop_householdtype_nochildren.value * 2.5;
+	
+	
+	d3.json("/api/v1.0/happyness_index", function (data) {
+	  console.log(data);
+	  dataset = data;
+	});
+	// TBD - perform your magical calculation
+	
+	
+	
+	bestCountriestoLiveIn = dataset.filter( c => c.happyscore >= ourscore );
+	
+	let magical_calculation_results = {
+		bestCountriestoLiveIn: bestCountriestoLiveIn,
+		geoDataforMap: {}
+	};
+	
+	// once you're done with the magical calculation...
   showOutput(magical_calculation_results);
 }
 
 function showOutput(magical_calculation_results) {
-	// generate some HTML (maybe through d3)
+	
+// generate some HTML (maybe through d3) - e.g Table
 	const countrylistlocation = document.getElementById("countryList");
 
 	let countries = magical_calculation_results.bestCountriestoLiveIn;
